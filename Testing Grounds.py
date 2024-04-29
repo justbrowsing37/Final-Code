b589544760949145f85abff3b120e7a305d5e1c1
justbrowsing37 <bhanusugguna@gmail.com>
2024-04-29 11:24:31 -0400

    starting work on the object classes

--- Testing Grounds.py
+++ Testing Grounds.py
@@ -1,14 +1,12 @@
-'''
+class Objects():
+    def thing1():
+        pass
+    
+    def thing2():
+        pass
 
-
-
-
-
-
-
-
-
-
-
-
-'''
+    def thing3():
+        pass
+    
+    def thing4():
+        pass
