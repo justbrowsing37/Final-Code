044bc9553ddbf851ecff7a29475d2fc8bbbaa73c
justbrowsing37 <bhanusugguna@gmail.com>
2024-06-06 12:44:57 -0400

    Fix the match cases

--- main.py
+++ main.py
@@ -15,58 +15,60 @@ stopwatch.start()
 while main_loop:
     #this is all pregame stuff
     game = input("Would you like to restore a previous game [y/n]: ").strip().lower()
-    if game == "y":
-        os.system('cls')
-        game_state = load_game()
-        if game_state is None:
-            print("\nNo previous game found...\nStarting a new game...")
-            disable.start()
-            time.sleep(3)
+
+    match game:
+        case"y":
+            os.system('cls')
+            game_state = load_game()
+            if game_state is None:
+                print("\nNo previous game found...\nStarting a new game...")
+                disable.start()
+                time.sleep(3)
+                game_state = GameState()
+                os.system('cls')
+                print_instructions()
+                disable.stop()
+        case "n":
             game_state = GameState()
             os.system('cls')
             print_instructions()
-            disable.stop()
-    elif game == "n":
-        game_state = GameState()
-        os.system('cls')
-        print_instructions()
-    else:
-        print("Invalid input. Please enter 'y' or 'n'.")
-        continue
+        case _:
+            print("Invalid input. Please enter 'y' or 'n'.")
+            continue
     while game_state.current_room:
         display_room_info(game_state.current_room, game_map)
         command = input("\nWhat would you like to do?: ").strip().lower()
-        if command == "save":
-            save_game(game_state)
-            print("Game saved.")
-            continue
-        if command == "inv":
-            inv_check(game_state)
-            continue
-        elif command == "help":
-            help(game_state)
-            continue
-        elif command == "quit":
-            if quit(game_state):
-                os.system('cls')
-                print("Quitting the game... \n\n\nGoodbye! \n\n")
-                main_loop = False
-                break  # Exit the game loop
-            else:
-                print("Resuming the game...")
-                continue  # Continue the game loop
+        
+        match command:
+            case "save":
+                save_game(game_state)
+                print("Game saved.")
+                continue
+            case"inv":
+                inv_check(game_state)
+                continue
+            case "help":
+                help(game_state)
+                continue
+            case "quit":
+                if quit(game_state):
+                    os.system('cls')
+                    print("Quitting the game... \n\n\nGoodbye! \n\n")
+                    main_loop = False
+                    break  # Exit the game loop
+                else:
+                    print("Resuming the game...")
+                    continue  # Continue the game loop
 
-        if command not in moves:
-            print(f"You don't know how to '{command}'")
-            separation()
-            continue
-        elif command == "take":
-            Take(game_state, game_map)
-            separation()
-        elif command == "move":
-            game_state = move(game_state, game_map,  directions)
-            separation()
-        elif command == "map":
-            map_check(game_state)
-        stopwatch.reset()
-        stopwatch.start()
+            case "take":
+                Take(game_state, game_map)
+                separation()
+            case "move":
+                game_state = move(game_state, game_map,  directions)
+                separation()
+            case "map":
+                map_check(game_state)
+            
+            case _:
+                print(f"You don't know how to '{command}'")
+                separation()
